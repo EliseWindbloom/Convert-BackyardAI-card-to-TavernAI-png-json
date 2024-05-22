171a5d206cb2945f8df04ba0abac37e7708d81a4
EliseWindbloom <109670213+EliseWindbloom@users.noreply.github.com>
2024-05-22 08:25:16 -0400

    Update _BackyardAI_To_TavernAI (drag & drop backyardAI png here).bat

--- _BackyardAI_To_TavernAI (drag & drop backyardAI png here).bat
+++ _BackyardAI_To_TavernAI (drag & drop backyardAI png here).bat
@@ -5,12 +5,9 @@ REM Check if an argument was provided
 if "%~1"=="" (
     echo Drag and drop an input png file onto this batch file.
     pause
-    exit /b
+    exit /b 1
 )
-
-REM Set the input file path
-set "input_file=%~1"
 
 cd /d %~dp0
 
-python "convert_backyard_to_tavern_v2.py" "!input_file!"
+python "convert_backyard_to_tavern_v3.py" %1
